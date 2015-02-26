4c14983f3bf490f870e488bb7ab0c163006cfe2d
gorkaeff <gorkajm85@gmail.com>
2015-02-26 14:58:34 +0100

    Change language (Spanish / English)

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,11 +1,15 @@
 'use strict';
 
 angular.module('myApp.menu', ['ngRoute'])
-.controller('MenuCtrl', function ($scope) {
+.controller('MenuCtrl', function ($scope, $translate) {
 	$scope.urlProject = 'https://github.com/gorkaeff/gorkaeff.github.io';
 	$scope.titleProject = "menu.titleProject";
 	$scope.generateMenu = [
 		{url : "#/", tag : "menu.home"},
 		{url : "#/contact", tag : "menu.contact"}
 	];
+
+	$scope.changeLang = function (lang){
+		$translate.use(lang);
+	}
 });
