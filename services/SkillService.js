f45577fcb26c4d404c6de70f42a40c7901d14237
gorkaeff <gorkajm85@gmail.com>
2015-02-27 12:54:08 +0100

    Description about skills multilanguage

--- services/SkillService.js
+++ services/SkillService.js
@@ -1,15 +1,31 @@
 angular.module('myApp.skillService', [])
 //---------------------------------------------------------------------------------------------------------------
-.factory('SkillService', function () {
+.factory('SkillService', function ($translate) {
 	var skills = [
-		{name : "AngularJS", value: 9},
-		{name : "Phonegap", value: 9},
-		{name : "Java", value: 8},
-		{name : "PHP", value: 6}
+		{
+			name : "AngularJS", 
+			value: 9,
+			description : "home.description_angularjs"
+		},
+		{
+			name : "Phonegap", 
+			value: 9,
+			description : "home.description_phonegap"
+		},
+		{
+			name : "Java", 
+			value: 8,
+			description : "home.description_java"
+		},
+		{
+			name : "PHP", 
+			value: 6,
+			description : "home.description_php"
+		}
 	];
 	
 	return {
-		getSkills : function(){
+		getSkills : function (){
 			return skills;
 		}
 	}
