2e54c8c6e736f22eab4bbdf1a09927a4550d5bf0
gorkaeff <gorkajm85@gmail.com>
2015-03-23 17:59:12 +0100

    Insert more skills and prepare android / iOS / github account

--- services/SkillService.js
+++ services/SkillService.js
@@ -1,26 +1,36 @@
 angular.module('myApp.skillService', [])
 //---------------------------------------------------------------------------------------------------------------
-.factory('SkillService', function ($translate) {
+.factory('SkillService', function () {
 	var skills = [
 		{
 			name : "AngularJS", 
-			value: 9,
+			value: 8,
 			description : "home.description_angularjs"
 		},
 		{
 			name : "Phonegap", 
-			value: 9,
+			value: 8,
 			description : "home.description_phonegap"
 		},
 		{
-			name : "Java", 
+			name : "Javascript", 
 			value: 8,
+			description : "home.description_javascript"
+		},
+		{
+			name : "Java", 
+			value: 7,
 			description : "home.description_java"
 		},
 		{
 			name : "PHP", 
 			value: 6,
 			description : "home.description_php"
+		},
+		{
+			name : "Ruby On Rails", 
+			value: 4,
+			description : "home.description_rails"
 		}
 	];
 	
