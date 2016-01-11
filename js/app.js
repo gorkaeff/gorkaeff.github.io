e93a8611af8535320766b7ef83b63025e737b7da
Gorkaeff <gorka.jusue@gmail.com>
2016-01-11 18:24:13 +0100

    Probar smartgit desde Ubuntu

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.menuCtrl',
-  'myApp.homeCtrl',
-  'myApp.contactCtrl',
-  'myApp.menuService',
-  'myApp.skillService',
-  'ui.bootstrap',
-  'myApp.directives',
-  'pascalprecht.translate'
+	'ngRoute',
+	'myApp.menuCtrl',
+	'myApp.homeCtrl',
+	'myApp.contactCtrl',
+	'myApp.menuService',
+	'myApp.skillService',
+	'ui.bootstrap',
+	'myApp.directives',
+	'pascalprecht.translate'
 ]).
 config(['$routeProvider', '$translateProvider', function($routeProvider, $translateProvider) {
 
@@ -19,11 +19,11 @@ config(['$routeProvider', '$translateProvider', function($routeProvider, $transl
 		suffix: '.json'
 	});
 
-  if (navigator.language.substring(0,2) == "es"){
-    $translateProvider.preferredLanguage('es');
-  } else {
-    $translateProvider.preferredLanguage('en');
-  }
+	if (navigator.language.substring(0,2) == "es"){
+		$translateProvider.preferredLanguage('es');
+	} else {
+		$translateProvider.preferredLanguage('en');
+	}
 
 	$routeProvider.otherwise({redirectTo: '/'});
 }]);
